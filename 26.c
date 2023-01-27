65a04d9920ddcc57161a88d112a505f6c093322a
chiyaangokul551 <120442068+chiyaangokul551@users.noreply.github.com>
2023-01-27 10:42:51 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -4,19 +4,17 @@
 int main( )
 {
  
-    // Declare the file pointer
+ 
     FILE *filePointer ;
      
-    // Get the data to be written in file
+   
     char dataToBeWritten[50]
         = "GeeksforGeeks-A Computer Science Portal for Geeks";
  
-    // Open the existing file GfgTest.c using fopen()
-    // in write mode using "w" attribute
+    
     filePointer = fopen("GfgTest.c", "w") ;
      
-    // Check if this filePointer is null
-    // which maybe if the file does not exist
+t
     if ( filePointer == NULL )
     {
         printf( "GfgTest.c file failed to open." ) ;
@@ -26,16 +24,15 @@ int main( )
          
         printf("The file is now opened.\n") ;
          
-        // Write the dataToBeWritten into the file
+   
         if ( strlen ( dataToBeWritten ) > 0 )
         {
-             
-            // writing in the file using fputs()
+         
             fputs(dataToBeWritten, filePointer) ;
             fputs("\n", filePointer) ;
         }
          
-        // Closing the file using fclose()
+        
         fclose(filePointer) ;
          
         printf("Data successfully written in file GfgTest.c\n");
